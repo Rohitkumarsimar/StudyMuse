25a62fb1a2603fe5b900734b30ab284861522a66
Rohit Kumar <162014017+Rohitkumarsimar@users.noreply.github.com>
2026-06-20 21:41:38 +0530

    added task route to server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,7 @@ import { logMethod } from "./src/middleware/logger.middleware.js";
 import { errorMiddleware } from "./src/middleware/error.middleware.js";
 import { authRouter } from "./src/routes/auth.routes.js";
 import { testRouter } from "./src/routes/test.routes.js";
+import { taskRouter } from "./src/routes/task.routes.js";
 
 const app = express();
 
@@ -18,7 +19,8 @@ app.use(express.json());
 
 app.use("/auth", authRouter);
 app.use("/test", testRouter);
-app.use(errorMiddleware)
+app.use("/tasks",taskRouter);
+app.use(errorMiddleware);
 
 app.listen(PORT || 3000, () => {
   console.log(`Server is connected to http://localhost:${PORT || 3000}`);
