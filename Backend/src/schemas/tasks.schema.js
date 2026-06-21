75278337773c7a102711d481f000830f050683ac
Rohit Kumar <162014017+Rohitkumarsimar@users.noreply.github.com>
2026-06-21 21:24:20 +0530

    feat: add task update schema with strict datetime validation for due_date

--- Backend/src/schemas/tasks.schema.js
+++ Backend/src/schemas/tasks.schema.js
@@ -3,5 +3,12 @@ import * as z from 'zod'
 export const createTaskSchema = z.object({
     title : z.string().min(3),
     subject : z.string().min(3),
-    due_date : z.string()
+    due_date : z.iso.datetime()
+}).strict()
+
+export const updateTaskSchema = z.object({
+    title : z.string().min(3).optional(),
+    subject : z.string().min(3).optional(),
+    due_date : z.iso.datetime().optional(),
+    is_completed:z.boolean().optional()
 }).strict()
