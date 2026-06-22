89db8533ebdc2dc00e771a2468f23d09ec03296c
Rohit Kumar <162014017+Rohitkumarsimar@users.noreply.github.com>
2026-06-22 13:35:36 +0530

    chore: remove test controller and routes

--- Backend/src/controllers/test.controller.js
+++
@@ -1,4 +0,0 @@
-// created test controller
-export async function testRoute(req, res, next) {
-  res.status(200).json({ test: "successfully executed." });
-}
--- Backend/src/routes/test.routes.js
+++
@@ -1,8 +0,0 @@
-import { testRoute } from "../controllers/test.controller.js";
-import { authMiddleware } from "../middleware/auth.middleware.js";
-import express from "express";
-
-// creating test router
-export const testRouter = express.Router();
-
-testRouter.get("/testApi", authMiddleware, testRoute);
