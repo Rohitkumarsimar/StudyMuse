f2b52817ddfd4a77cb37f3277de9bb665c9394f9
Rohit Kumar <162014017+Rohitkumarsimar@users.noreply.github.com>
2026-07-01 23:43:04 +0530

    feat: separated api call into another file, maintaining the architecture

--- Backend/src/services/chat.service.js
+++ Backend/src/services/chat.service.js
@@ -8,6 +8,8 @@ import {
 } from "../db/chat.query";
 import { ApiError } from "../utils/AppError";
 
+import { main } from "../utils/aiApi";
+
 export async function getConversationsService(user_id) {
   const result = await getAllConversations(user_id);
   return result;
@@ -18,25 +20,25 @@ export async function getMessagesService(user_id, conv_id) {
   return result;
 }
 
-export async function createConversationService(
+export async function startConversationService(
   user_id,
   conv_id,
   role,
   content,
 ) {
- 
-    if (content.trim()) {
-      throw new ApiError(400, "Enter a valid message!");
-    }
-    if (content.length > 1000) {
-      throw new ApiError(400, "Propt length is exceeding the limit!");
-    }
-    
-    const messageHistory = await getMessages(user_id, conv_id)
+  if (content && content.trim().length < 1) {
+    throw new ApiError(400, "Enter a valid message!");
+  }
+  if (content.length > 1000) {
+    throw new ApiError(400, "Propt length is exceeding the limit!");
+  }
 
-    const history = messageHistory.map((role, content)=>({role: role, content: content}))
+  const history = await getMessages(user_id, conv_id);
 
-    const isFirstMessage = history.length === 0 
+  const messageHistory = history.map((element) => {
+    const { role, content } = element;
+    return { role, content };
+  });
 
-    const message = ["System prompt", {history}, {role, content}]
+  main()
 }
