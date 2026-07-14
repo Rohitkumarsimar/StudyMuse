56be6f357df933ac51415f53426450384f51744c
Rohit Kumar <162014017+Rohitkumarsimar@users.noreply.github.com>
2026-07-14 20:57:01 +0530

    add: seed.js to seed static data

---
+++ Backend/prisma/seed.js
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+
+async function main(){
+    await prisma.board.create({
+        data:{
+            name:"CBSE"
+        }
+    })
+}
+
+main()
+.then(async ()=>{
+    await prisma.$disconnect();
+})
+.catch(async (e)=>{
+    console.log(e)
+    await prisma.$disconnect()
+    process.exit(1)
+})
