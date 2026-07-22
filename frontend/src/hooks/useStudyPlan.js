7cfb53d2b78e19d96ffcc4308090b5c801194c94
Rohit Kumar <162014017+Rohitkumarsimar@users.noreply.github.com>
2026-07-22 23:23:22 +0530

    feat: Made custom hooks: useStudyPlan.js, useDebounce.js. Created a search studyplan feature using fuse.js

--- frontend/src/hooks/useStudyPlan.js
+++ frontend/src/hooks/useStudyPlan.js
@@ -1,52 +1,105 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { api } from "../api/axios.js";
+import Fuse from "fuse.js";
+import { useDebounce } from "./useDebounce.js";
 
-/*{
-    useStudyPlans
+export function useStudyPlan() {
+  const [studyPlans, setStudyPlans] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-├── getAllStudyPlans()
-├── createStudyPlan()
-├── updateStudyPlan()
-├── deleteStudyPlan()
-├── searchStudyPlans()
-├── loading
-├── error
-└── studyPlans state
-}*/
-
-export function useStudyPlan(){
-    const [studyPlans, setStudyPlans] = useState([])
-    const [error, setError] = useState(null)
-    const [isLoading, setIsLoading] = useState(false)
-
-    async function getAllStudyPlans(){
-        setError(null)
-        try{
-            setIsLoading(true)
-            const result = await api.get('/studyPlan')
-            setStudyPlans(result.data.data)
-        }catch(err){
-            setError(err)
-        }finally{
-            setIsLoading(false)
-        }
+  async function getAllStudyPlans() {
+    setError(null);
+    try {
+      setIsLoading(true);
+      const result = await api.get("/studyPlan");
+      setStudyPlans(result.data.data);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
     }
+  }
 
-    useEffect(()=>{
-        getAllStudyPlans()
-    },[])
+  useEffect(() => {
+    getAllStudyPlans();
+  }, []);
 
-    async function createStudyPlan(data){
-        setError(null)
-        try{
-            setIsLoading(true)
-            const result = await api.post('/studyPlan',data)
-            setStudyPlans(prev=>[...prev,result.data.data])
-        }catch(err){
-            setError(err)
-        }finally{
-            setIsLoading(false)
-        }
+  async function createStudyPlan(data) {
+    setError(null);
+    try {
+      setIsLoading(true);
+      const result = await api.post("/studyPlan", data);
+      setStudyPlans((prev) => [...prev, result.data.data]);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
     }
+  }
 
+  async function updateStudyPlan(id, data) {
+    setError(null);
+    try {
+      setIsLoading(true);
+      const result = await api.patch(`/studyPlan/${id}`, data);
+
+      const updatedStudyPlan = studyPlans.map((plan) => {
+        if (plan.id === id) {
+          return result.data.data;
+        }
+        return plan;
+      });
+
+      setStudyPlans(updatedStudyPlan);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function deleteStudyPlan(id) {
+    setError(null);
+    try {
+      setIsLoading(true);
+      await api.delete(`/studyPlan/${id}`);
+      const newStudyPlan = studyPlans.filter((plan) => plan.id === id);
+      setStudyPlans(newStudyPlan);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  // search query
+  const [searchQuery, setSearchQuery] = useState("");
+
+    const debouncedSearchQuery = useDebounce(searchQuery, 300);
+    const fuse = useMemo(() => {
+      return new Fuse(studyPlans, {
+        keys: ["title", "description"],
+      });
+    }, [studyPlans]);
+
+    const hasSearchQuery = debouncedSearchQuery.trim().length > 0;
+    const searchResult = hasSearchQuery
+      ? fuse.search(debouncedSearchQuery)
+      : [];
+    const filteredStudyPlans = hasSearchQuery
+      ? searchResult.map(({ item }) => item)
+      : studyPlans;
+
+
+  return {
+    getAllStudyPlans,
+    createStudyPlan,
+    updateStudyPlan,
+    deleteStudyPlan,
+   
+   studyPlans: filteredStudyPlans,
+    searchQuery,
+    setSearchQuery
+  };
 }
