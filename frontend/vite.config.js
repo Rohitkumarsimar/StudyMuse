2e66b0ad30649a1a6110497815d7d05c17bed831
Rohit Kumar <162014017+Rohitkumarsimar@users.noreply.github.com>
2026-07-28 22:49:25 +0530

    added manifest.webmanifast for pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        id: "/",
+        name: "StudyMuse",
+        short_name: "StudyMuse",
+        description: "AI-powered study management platform",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        orientation: "portrait",
+        theme_color: "#010931",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
     },
   },
-   server: {
+
+  server: {
     host: true,
-  }
-})
+  },
+});
