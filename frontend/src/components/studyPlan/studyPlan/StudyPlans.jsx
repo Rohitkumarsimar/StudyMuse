ee4be80afdc6dfc3784fae0acfc8022938ecfba6
Rohit Kumar <162014017+Rohitkumarsimar@users.noreply.github.com>
2026-07-28 21:08:46 +0530

    scrollbar fix

--- frontend/src/components/studyPlan/studyPlan/StudyPlans.jsx
+++ frontend/src/components/studyPlan/studyPlan/StudyPlans.jsx
@@ -2,7 +2,7 @@ import { StudyPlanItems } from "./StudyPlanItems";
 export function StudyPlans({studyPlans}) {
 
   return (
-    <div className="p-7  border border-gray-200 bg-gray-50 rounded-2xl h-full w-full overflow-y-scroll ">
+    <div className="p-7  border border-gray-200 bg-gray-50 rounded-2xl h-full w-full overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-200">
       {studyPlans.map((studyPlan) => (
         <StudyPlanItems
           key={studyPlan.id}
