cdc8dd0662506c52bec435bd8d460bb9891f2213
Rohit Kumar <162014017+Rohitkumarsimar@users.noreply.github.com>
2026-07-28 23:35:52 +0530

    feat: add Android TWA support and release build

---
+++ frontend/app/src/main/java/com/rohit/studymuse/DelegationService.java
@@ -0,0 +1,14 @@
+package com.rohit.studymuse;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
